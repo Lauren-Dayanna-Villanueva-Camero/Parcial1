ea29f989f91d49ed8ba92f3f7f849fbd2b38726c
Lauren Villanueva <u20241220012@usco.edu.com>
2025-09-13 09:11:10 -0500

    Agregar cartas a mision y vision y cambios en js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,25 @@
       console.log("Por favor complete los campos.");
     }
   });
+
+
+  // Misión y vision
+  const cardText = document.getElementById("cardText");
+  const btnSize = document.getElementById("btnSize");
+  const btnColor = document.getElementById("btnColor");
+
+  btnSize.addEventListener("click", () => {
+    if (cardText.style.fontSize === "20px") {
+      cardText.style.fontSize = "14px"; 
+    } else {
+      cardText.style.fontSize = "20px";
+    }
+  });
+
+  btnColor.addEventListener("click", () => {
+    if (cardText.style.color === "red") {
+      cardText.style.color = "black"; 
+    } else {
+      cardText.style.color = "red"; 
+    }
+  });
